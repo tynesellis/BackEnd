7d2e7621a89b900866f594cdf99d38ef7742833b
Paul Ellis <tynesellis@gmail.com>
2018-03-20 14:42:45 -0700

    Fixed sign in bug

--- HulduNashville/Controllers/AccountController.cs
+++ HulduNashville/Controllers/AccountController.cs
@@ -61,7 +61,9 @@ namespace HulduNashville.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                ApplicationUser ThisUser = await _userManager.FindByEmailAsync(model.Email);
+               
+                var result = await _signInManager.PasswordSignInAsync(ThisUser.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");

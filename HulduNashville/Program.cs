f7624c5fc1f0779880607a955e615d52a04da4f1
Paul Ellis <tynesellis@gmail.com>
2018-03-21 19:28:22 +0000

    Bundler Added

--- HulduNashville/Program.cs
+++ HulduNashville/Program.cs
@@ -43,6 +43,7 @@ namespace HulduNashville
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
